ee3b9ccf4f03b09ade4fdf25fd16ba6a72e5c720
deadguru123 <abhay123bha@gmail.com>
2026-03-14 00:36:57 +0530

    intersection ponint of two linkedlist.

--- linkedlist/intersection_point_of_LL.cpp
+++ linkedlist/intersection_point_of_LL.cpp
@@ -20,3 +20,30 @@ public:
         return NULL;
     }
 };
+//optimal approach
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+       if(temp1==NULL and temp2==NULL)
+       return NULL;
+        while(temp1!=temp2)
+        {
+            
+            temp1=temp1->next;
+            temp2=temp2->next;
+            if(temp1==temp2)
+            return temp2;
+            if(temp1==NULL)
+            temp1=headB;
+             if(temp2==NULL)
+            temp2=headA;
+
+
+        }
+       
+        return temp1;
+    }
+};
