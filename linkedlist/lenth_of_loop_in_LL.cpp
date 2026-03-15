456ec0794bb0b601b94ec812f4ed5b22fe46280e
deadguru123 <abhay123bha@gmail.com>
2026-03-15 22:53:13 +0530

    loop length of LL

--- linkedlist/lenth_of_loop_in_LL.cpp
+++ linkedlist/lenth_of_loop_in_LL.cpp
@@ -16,3 +16,31 @@ while(temp)
 }
     }
 };
+// without using hashing
+int findlength(Node * slow,Node*fast)
+{
+    int c=1;
+    Node *fa=fast->next;
+    while(slow!=fa)
+    {
+        c++;
+        fa=fa->next;
+    }
+    return c;
+}
+class Solution {
+  public:
+
+    int lengthOfLoop(Node *head) {
+        Node *slow=head;
+        Node *fast=head;
+        while(fast!=NULL && fast->next!=NULL)
+{
+    slow=slow->next;
+    fast=fast->next->next;
+    if(slow==fast)
+    return findlength(slow,fast);
+    }
+    return 0;
+    }
+};
