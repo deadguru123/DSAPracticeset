8cfb59da09c1d2fab1cd9d104d9847d133c69ecb
deadguru123 <abhay123bha@gmail.com>
2026-04-20 23:34:52 +0530

    max ones 3 on leetcode

---
+++ slidingwindow/maxones.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int maxlen=0,l=0,r=0,zeros=0,len=0;
+        int n=nums.size();
+        while(r<n)
+        {
+            if(nums[r]==0)
+            zeros++;
+            while(zeros>k)
+            {
+                if(nums[l]==0)
+                zeros--;
+                l++;
+            }
+            if(zeros<=k)
+            {
+            len=r-l+1;
+            maxlen=max(maxlen,len);
+            }
+            r++;
+
+        }
+        return maxlen;
+
+    }
+};
