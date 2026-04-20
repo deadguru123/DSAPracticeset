e4d817b2d879cfca8d1b14048f3492465e14dab1
deadguru123 <abhay123bha@gmail.com>
2026-04-20 23:47:55 +0530

    max ones 3 on leetcode

--- slidingwindow/maxones.cpp
+++ slidingwindow/maxones.cpp
@@ -25,3 +25,36 @@ public:
 
     }
 };
+//upr wala tarika O(n2) ka hai
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int maxlen=0,l=0,r=0,zeros=0,len=0;
+        int n=nums.size();
+        while(r<n)
+        {
+            if(nums[r]==0)
+            zeros++;
+             if(zeros>k)
+            {
+                
+                if(nums[l]==0)
+                zeros--;
+                l++;
+            }
+                if(zeros<=k)
+                {
+                    len=r-l+1;
+                maxlen=max(maxlen,len);
+
+                }
+            
+           
+            
+            r++;
+
+        }
+        return maxlen;
+
+    }
+};
